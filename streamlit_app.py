7326b569bc812998d48990de5f9566d5a9641407
joseduenez <106273387+joseduenez@users.noreply.github.com>
2024-02-25 15:37:17 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,23 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+import yfinance as yf
 import streamlit as st
+import pandas as pd
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.write(""" # Simple Stock Price App
+         
+         Shown are the stock closing price and volume of Google!
+         
+         """)
+#define the ticker symbol
+tickerSymbol = "GOOGL"
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period="1d", start="2010-5-31", end="2020-5-31")
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+#open high     low close      volume      dividends    stock splits
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.volume)
